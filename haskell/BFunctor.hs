59e5b2f0a9d766851bba00463d5325c774398eff
Brent Yorgey <byorgey@cis.upenn.edu>
2013-06-05 16:16:24 -0400

    move liftIso from BFunctor to Iso

--- haskell/BFunctor.hs
+++ haskell/BFunctor.hs
@@ -17,8 +17,3 @@ class BFunctor f where
   -- are BFunctors but not Functors.)
   default bmap :: Functor f => (a <-> b) -> (f a <-> f b)
   bmap g = mapping g
-
--- Lift an iso on a field to an iso of the whole structure
-liftIso :: Setter s t a b -> Setter t s b a -> (a <-> b) -> (s <-> t)
-liftIso l1 l2 ab = withIso ab $ \f g -> iso (l1 %~ f) (l2 %~ g)
-
--- haskell/Iso.hs
+++ haskell/Iso.hs
@@ -9,3 +9,6 @@ type (<->) a b = Iso' a b
 
 type (<-->) f g = forall l. f l <-> g l
 
+-- Lift an iso on a field to an iso of the whole structure.
+liftIso :: Setter s t a b -> Setter t s b a -> (a <-> b) -> (s <-> t)
+liftIso l1 l2 ab = withIso ab $ \f g -> iso (l1 %~ f) (l2 %~ g)
