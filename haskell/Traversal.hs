37bebb927acc4453daec40a924d8e28403ec1720
Brent Yorgey <byorgey@cis.upenn.edu>
2013-06-19 13:39:06 -0400

    work on fromTrav a bit

--- haskell/Traversal.hs
+++ haskell/Traversal.hs
@@ -8,20 +8,43 @@
 module Traversal where
 
 import SpeciesTypes
-import Data.Traversable
+import qualified Data.Traversable as T
 import qualified Data.Foldable as F
 import Control.Applicative
 import Finite
 import qualified Vec as V
-import qualified Prelude as P
 
-{- we would like:
-fromTrav :: (F.Foldable f) => f a -> Sp' (f # L) a
-but we can at least get:
--}
+import Control.Monad.Writer
+import Control.Monad.Supply
+
+-- Use this orphan instance until
+-- https://github.com/ghulette/monad-supply/pull/3 is merged and
+-- released
+instance Applicative (Supply s) where
+  pure  = return
+  (<*>) = ap
+
 fromFold :: F.Foldable f => f a -> Sp' L a
 fromFold f = fromList l
   where l = F.foldr (:) [] f
+
+fromTrav :: T.Traversable f => f a -> Sp' (f # L) a
+fromTrav = mkSp' . T.traverse replace
+  where
+    replace :: a -> WriterT [a] (Supply Int) Int
+    replace a = do
+      l <- supply
+      tell [a]
+      return l
+    mkSp' m =
+      let (fl, as) = flip evalSupply [0..] . runWriterT $ m
+      in  undefined
+          -- case V.fromList as of
+          --   (V.SomeVec v) ->
+          --     SpEx (Struct (Shape (fmap undefined fl)) v)
+
+        -- convert all Ints to Fin n for some n, convert as to vector,
+        -- pair up with L shape
 
 -- All of these are valid:
 instance Finite l => F.Foldable (Sp L l) where
