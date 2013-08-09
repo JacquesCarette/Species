80554610e5333d070ae222640b1c3e7d4115fe40
Jacques Carette <carette@mcmaster.ca>
2013-08-09 15:44:08 -0400

    take completely implemented

--- haskell/VecLike.hs
+++ haskell/VecLike.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
 
 ------------------------------------------
 -- The point of this module is to show that many vector operations
@@ -16,11 +18,21 @@ import qualified Vec as V
 import FinIsos
 import Control.Lens
 import qualified Set as S
+import Proxy
+import Equality
 
-take :: forall f l a n . (N.Natural n, Finite l) => Sp f l a -> N.SNat n -> (n <= Size l) -> 
+natty :: N.SNat n -> (N.Natural n => r) -> r
+natty N.SZ r     = r
+natty (N.SS n) r = natty n r
+
+take :: forall f l a n . (Finite l) => Sp f l a -> N.SNat n -> (n <= Size l) -> 
   Sp (f # Part) l a
-take (Struct f i) n pf = Struct (cprodSh f (Shape k)) i
-  where k :: Part l
-        k = Part (S.enumerate :: S.Set (N.Fin n)) (S.enumerate) isom
-        isom :: Either (N.Fin n) l2 <-> l
-        isom = undefined
+take (Struct f i) n pf = 
+  case minus (size (Proxy :: Proxy l)) n pf of
+    Minus (m :: N.SNat m) Refl -> 
+      case plusComm m n of 
+        Refl -> Struct (cprodSh f (Shape k)) i
+          where k :: Part l
+                k = natty n $ natty m $ Part S.enumerate S.enumerate isom
+                isom :: Either (N.Fin n) (N.Fin m) <-> l
+                isom = iso (finSum n m) (finSumInv n m) . finite
