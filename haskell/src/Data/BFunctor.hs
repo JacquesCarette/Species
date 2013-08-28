2b76b3a2125890f51df0045aaa5dadb44a7625c1
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-28 15:24:42 -0400

    documentation for BFunctor

--- haskell/src/Data/BFunctor.hs
+++ haskell/src/Data/BFunctor.hs
@@ -7,12 +7,14 @@ module Data.BFunctor where
 import Control.Lens
 import Data.Finite
 
--- Functors in the category B of finite sets with isomorphisms as
--- arrows.
+-- | Functors in the category B of finite sets with isomorphisms as
+--   arrows.
+--
+--   Note that @bmap@ has a default implementation in terms of 'fmap'
+--   for @f@ which are also an instance of @Functor@ (however,
+--   not all @BFunctor@s are @Functor@s.)
 class BFunctor f where
   bmap :: (Finite a, Finite b) => (a <-> b) -> (f a <-> f b)
 
-  -- Any Functor is automatically a BFunctor. (However, some things
-  -- are BFunctors but not Functors.)
   default bmap :: Functor f => (a <-> b) -> (f a <-> f b)
   bmap g = mapping g
