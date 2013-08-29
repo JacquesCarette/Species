231d5d52f9cf76c366436debf4615946f5fcec46
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-29 15:37:28 -0400

    documentation for Data.Set.Abstract

--- haskell/src/Data/Set/Abstract.hs
+++ haskell/src/Data/Set/Abstract.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+-- | A simple model of abstract (mathematical) sets.
 module Data.Set.Abstract (Set, enumerate, elimSet) where
 
 import           Control.Lens
@@ -9,6 +10,8 @@ import           Data.Fin
 import           Data.Finite
 import           Data.Proxy
 
+-- | A set is an unordered collection of elements in which each
+--   element occurs at most once.
 newtype Set a = Set [a]
   deriving Show
 
@@ -16,10 +19,13 @@ instance BFunctor Set where
   bmap i = iso (\(Set as) -> Set (map (view i) as))
                (\(Set as) -> Set (map (view (from i)) as))
 
+-- | The set of elements of a 'Finite' type.
 enumerate :: forall l. Finite l => Set l
 enumerate = Set $ map (view finite) (fins (size (Proxy :: Proxy l)))
 
--- | Proof obligation: the first argument is a function which yields
---   the same result for any permutation of a given input list.
+-- | Generic eliminator for 'Set'. Note that using @elimSet' incurs a
+--   proof obligation, namely, the first argument must be a function
+--   which yields the same result for any permutation of a given input
+--   list.
 elimSet :: ([a] -> b) -> Set a -> b
 elimSet f (Set xs) = f xs
