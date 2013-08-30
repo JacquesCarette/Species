1e17903283be28572ebb4f1b106670b3e8ff6109
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-29 20:31:37 -0400

    clean up + documentation for Data.Species.Isos

--- haskell/src/Data/Species/Isos.hs
+++ haskell/src/Data/Species/Isos.hs
@@ -2,7 +2,15 @@
 {-# LANGUAGE Rank2Types    #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Data.Species.Isos where
+-- | Some isomorphisms of species.  Note that only isomorphisms of
+--   labelled shapes are provided; lifting these to isomorphisms of
+--   labelled structures can be accomplished with 'relabel' and
+--   'relabel''.
+module Data.Species.Isos
+    ( zeroCancel, oneCancel
+    , sumComm, prodComm
+
+    ) where
 
 import           Control.Lens
 
@@ -11,16 +19,13 @@ import           Data.Finite
 import           Data.Species.Types
 import           Data.Type.Isos
 
-import           Unsafe.Coerce
-
-absurdS :: Zero l -> a
-absurdS = unsafeCoerce
+-- XXX need to add a bunch more!
 
 zeroCancel :: (Zero * f) <--> Zero
-zeroCancel = iso elimZf absurdS
+zeroCancel = iso elimZf absurdZ
              where
                elimZf :: (Zero * f) l -> a
-               elimZf (Prod x y i) = absurdS x
+               elimZf (Prod x _ _) = absurdZ x
 
 oneCancel :: BFunctor f => (One * f) <--> f
 oneCancel = iso
@@ -28,22 +33,13 @@ oneCancel = iso
                            {- l2 ~ Either Void l2 ~  Either l1 l2 ~ l -}
   (\fl -> Prod (One id) fl zeroPL)
 
-plusComm :: (f + g) <--> (g + f)
-plusComm = iso swap swap
+sumComm :: (f + g) <--> (g + f)
+sumComm = iso swap swap
            where
              swap (Inl x) = Inr x
              swap (Inr x) = Inl x
 
-timesComm :: (f * g) <--> (g * f)
-timesComm = iso swap swap
+prodComm :: (f * g) <--> (g * f)
+prodComm = iso swap swap
              where
                swap (Prod x y i) = Prod y x (commP . i)
-
-------------------------------
--- Some species isomorphisms
-
-isoPlus :: Finite l => Sp (f + g) l a <-> Sp (g + f) l a
-isoPlus = reshape' plusComm
-
-isoTimes :: Finite l => Sp (f * g) l a <-> Sp (g * f) l a
-isoTimes = reshape' timesComm
