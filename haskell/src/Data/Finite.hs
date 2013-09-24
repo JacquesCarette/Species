e9ba49035c3f990ba26b0622d3be555b87cf8e20
Jacques Carette <carette@mcmaster.ca>
2013-09-24 09:56:56 -0400

    split Finite into HasSize and Finite (part of a larger refactor)

--- haskell/src/Data/Finite.hs
+++ haskell/src/Data/Finite.hs
@@ -14,7 +14,8 @@ module Data.Finite
      type (<->), liftIso, isoPresSize, type (-->), type (<-->)
 
       -- * Constructively finite types
-    , Finite(..)
+    , HasSize(..), Finite(..)
+    , toFin, fromFin
     )
     where
 
@@ -68,6 +69,13 @@ type (-->) f g = forall l. (Eq l, Finite l) => f l -> g l
 ------------------------------------------------------------
 --  Constructively finite types
 
+class HasSize l where
+  type Size l :: Nat
+  -- ^ An type family giving the size of @l@.
+
+  size        :: Proxy l -> SNat (Size l)
+  -- ^ Get the size of a finite type.
+
 -- | Constructively finite types.
 --
 --   Currently, a finite type is defined as a type @l@ for which there
@@ -78,45 +86,47 @@ type (-->) f g = forall l. (Eq l, Finite l) => f l -> g l
 --   Abstractly, however, the notions of finiteness and linear
 --   orderings ought to be orthogonal.  We must be careful to note
 --   when we are taking advantage of this implicit ordering.
-class Eq l => Finite l where
-
-  type Size l :: Nat
-  -- ^ An associated type family giving the size of @l@.
-
-  size        :: Proxy l -> SNat (Size l)
-  -- ^ Get the size of a finite type.
+class (Eq l, HasSize l) => Finite l where
 
   finite      :: Fin (Size l) <-> l
   -- ^ Isomorphism witnessing the finiteness of @l@.
 
-  toFin       :: l -> Fin (Size l)
-  -- ^ One direction of the isomorphism as a function, provided for
-  --   convenience.
-  toFin = view (from finite)
+toFin       :: Finite l => l -> Fin (Size l)
+-- ^ One direction of the isomorphism as a function, provided for
+--   convenience.
+toFin = view (from finite)
 
-  fromFin     :: Fin (Size l) -> l
-  -- ^ The other direction of the isomorphism as a function, provided
-  --   for convenience.
-  fromFin = view finite
+fromFin     :: Finite l => Fin (Size l) -> l
+-- ^ The other direction of the isomorphism as a function, provided
+--   for convenience.
+fromFin = view finite
 
-instance Natural n => Finite (Fin n) where
+instance Natural n => HasSize (Fin n) where
   type Size (Fin n) = n
   size _ = toSNat
+
+instance Natural n => Finite (Fin n) where
   finite = id
 
-instance Finite DV.Void where
+instance HasSize DV.Void where
   type Size DV.Void = Z
   size _ = SZ
+
+instance Finite DV.Void where
   finite = iso absurd DV.absurd
 
-instance Finite () where
+instance HasSize () where
   type Size () = S Z
   size _ = SS SZ
+
+instance Finite () where
   finite = iso (const ()) (const FZ)
 
-instance Finite a => Finite (Maybe a) where
+instance HasSize a => HasSize (Maybe a) where
   type Size (Maybe a) = S (Size a)
   size _ = SS (size (Proxy :: Proxy a))
+
+instance Finite a => Finite (Maybe a) where
   finite = iso toM fromM
     where
       toM :: Fin (S (Size a)) -> Maybe a
@@ -127,24 +137,30 @@ instance Finite a => Finite (Maybe a) where
       fromM Nothing  = FZ
       fromM (Just l) = FS (toFin l)
 
-instance Finite Bool where
+instance HasSize Bool where
   type Size (Bool) = S (S Z)
   size _ = SS (SS SZ)
+
+instance Finite Bool where
   finite = iso (\s -> case s of FZ -> False; FS FZ -> True)
                (\b -> case b of False -> FZ; True -> FS FZ)
 
-instance (Finite a, Finite b) => Finite (Either a b) where
+instance (HasSize a, HasSize b) => HasSize (Either a b) where
   type Size (Either a b) = Plus (Size a) (Size b)
   size _ = plus (size (Proxy :: Proxy a)) (size (Proxy :: Proxy b))
+
+instance (Finite a, Finite b) => Finite (Either a b) where
   finite = iso ((fromFin +++ fromFin) . finSum' szA szB)
                (finSum szA szB . (toFin +++ toFin))
     where
       szA = size (Proxy :: Proxy a)
       szB = size (Proxy :: Proxy b)
 
-instance (Finite a, Finite b) => Finite (a,b) where
+instance (HasSize a, HasSize b) => HasSize (a,b) where
   type Size (a,b) = Times (Size a) (Size b)
   size _ = times (size (Proxy :: Proxy a)) (size (Proxy :: Proxy b))
+
+instance (Finite a, Finite b) => Finite (a,b) where
   finite = iso ((fromFin *** fromFin) . finProd' szA szB)
                (finProd szA szB . (toFin *** toFin))
     where
