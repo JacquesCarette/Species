01ceeab9be4c8e5ee8c9ebdc872b5a0f0a8bc182
Brent Yorgey <byorgey@cis.upenn.edu>
2013-10-02 14:31:40 -0400

    implementation of (a simple form of) the Garsia-Milne involution principle

--- haskell/src/Data/Iso.hs
+++ haskell/src/Data/Iso.hs
@@ -7,6 +7,7 @@ module Data.Iso
 
      type (<->), liftIso, type (-->), type (<-->)
 
+    , subtractIso
     )
     where
 
@@ -41,6 +42,21 @@ type (<-->) f g = forall l. Eq l => f l <-> g l
 --   > liftIso _1 _1 :: (a <-> b) -> ((a,c) <-> (b,c))
 liftIso :: Setter s t a b -> Setter t s b a -> (a <-> b) -> (s <-> t)
 liftIso l1 l2 ab = withIso ab $ \f g -> iso (l1 %~ f) (l2 %~ g)
+
+-- | A simplified variant of the Garsia-Milne involution principle.
+--   If we have an iso (A <-> B) and an iso (A+C <-> B+D), then we can
+--   \"subtract" the first from the second to obtain an explicit iso
+--   (C <-> D).
+--
+--   XXX draw a picture to illustrate how this works
+subtractIso :: (a <-> b) -> (Either a c <-> Either b d) -> (c <-> d)
+subtractIso ab e = iso (iter ab e . Right) (iter (from ab) (from e) . Right)
+  where
+    iter :: (a <-> b) -> (Either a c <-> Either b d) -> Either a c -> d
+    iter ab e eac
+      = case view e eac of
+          Right d -> d
+          Left  b -> iter ab e (Left (view (from ab) b))
 
 ------------------------------------------------------------
 --  Natural transformations
