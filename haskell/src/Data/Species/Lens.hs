4950f51b12d9073aa05786a1bf19f6a47dabbce4
Jacques Carette <carette@mcmaster.ca>
2013-10-09 22:21:11 -0400

    implement lens for Sp

---
+++ haskell/src/Data/Species/Lens.hs
@@ -0,0 +1,17 @@
+{- Lenses into Species -}
+{-# LANGUAGE RankNTypes #-}
+module Data.Species.Lens where
+
+import           Control.Lens
+
+import           Data.Finite
+import           Data.Species.Types (Sp(..))
+import qualified Data.Vec           as V
+
+lensSp :: l -> Lens' (Sp f l a) a
+lensSp lbl = 
+    lens (\(Struct _ e finl) -> V.index e $ toFin finl lbl)
+         (\(Struct sh e finl) a -> 
+             Struct sh (V.replace (toFin finl lbl) a e) finl)
+
+
