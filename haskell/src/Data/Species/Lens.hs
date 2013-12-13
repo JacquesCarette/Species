10a99b56e7b53e0570c796851350636b1e51479e
Jacques Carette <carette@mcmaster.ca>
2013-12-12 21:55:43 -0500

    fix Data.Species.Lens.  New Storage class simplifies this a fair bit

--- haskell/src/Data/Species/Lens.hs
+++ haskell/src/Data/Species/Lens.hs
@@ -1,5 +1,5 @@
 {- Lenses into Species -}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, ConstraintKinds #-}
 module Data.Species.Lens where
 
 import           Control.Lens
@@ -7,10 +7,9 @@ import           Control.Lens
 import           Data.Finite
 import           Data.Species.Shape (P(..))
 import           Data.Species.Types (Sp(..))
-import qualified Data.Vec           as V
+import qualified Data.Storage as S
 
-lensSp :: l -> Lens' (Sp f l a) a
+lensSp :: (S.Storage s, S.LabelConstraint s l) => l -> Lens' (Sp f s l a) a
 lensSp lbl = 
-    lens (\(Struct _ e finl) -> V.index e $ toFin finl lbl)
-         (\(Struct sh e finl) a -> 
-             Struct sh (V.replace (toFin finl lbl) a e) finl)
+    lens (\(Struct _ e) -> S.index e lbl)
+         (\(Struct sh e) a -> Struct sh (snd $ S.replace lbl a e))
