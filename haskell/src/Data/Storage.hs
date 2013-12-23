99723111284ae6aabb21e32530fb200a7bd8ebc8
Jacques Carette <carette@mcmaster.ca>
2013-12-23 15:47:42 -0500

    partial implementation of finite maps as another method of storage, but this disturbs too many things, get back to it later.

--- haskell/src/Data/Storage.hs
+++ haskell/src/Data/Storage.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE GADTs                 #-}
 
 module Data.Storage
     ( Storage(..)
@@ -15,8 +16,12 @@ module Data.Storage
 import Prelude hiding (zip, zipWith, concat)
 import GHC.Exts
 
-import Control.Lens (review)
+import Control.Lens (review,retagged)
 import Control.Applicative (liftA2)
+
+import qualified Data.HashMap.Lazy as HM
+import Data.HashMap.Lazy ((!))
+import Data.Hashable
 
 import Data.Type.Nat
 import Data.Fin
@@ -60,7 +65,7 @@ class Storage s where
 
   -- | Collapse nested blocks of storage into one, taking the pair of
   --   their label types.
-  concat :: s l1 (s l2 a) -> s (l1,l2) a
+--  concat :: s l1 (s l2 a) -> s (l1,l2) a
 
   -- | Initialize a potentially infinite block of storage with some
   --   initial content.  This is experimental.
@@ -80,5 +85,44 @@ instance Storage (->) where
   smap                  = (.)
   append f _ (Left l1)  = f l1
   append _ g (Right l2) = g l2
-  concat                = uncurry
+--  concat                = uncurry
   initialize f          = f
+
+{- 
+   Comment out this for now, will get back to it later.
+
+-- A HashMap can *almost* be made an instance of Storage, save for a
+-- sane way to implement reindex.  And since that is rather crucial,
+-- we have to wrap things up so that it can be made to work.
+data FinHashMap l v where
+   FHM :: (HasSize l, Hashable l, Eq l) => 
+               Finite l -> HM.HashMap l v -> FinHashMap l v
+
+-- allocation requires real work.
+alloc_fhm :: (HasSize l, Hashable l, Eq l) => Finite l -> (l -> a) -> HM.HashMap l a
+alloc_fhm fin f = foldr (\l m -> HM.insert l (f l) m) HM.empty 
+    (map (fromFin fin) (fins (size fin)))
+
+-- changing keys is work too.
+rekey :: (Hashable l', Eq l') => (l -> l') -> HM.HashMap l a -> HM.HashMap l' a
+rekey f m = HM.foldrWithKey (\k v nm -> HM.insert (f k) v nm) HM.empty m
+
+instance Storage FinHashMap where
+  type LabelConstraint FinHashMap l = (Eq l, Hashable l, HasSize l)
+  allocate fin f                   = FHM fin (alloc_fhm fin f)
+{-
+  reindex sub (FHM fin h) = FHM (retagged (asPIso sub)) h
+-}
+  index (FHM fin h) l              = h ! l
+  replace l a (FHM fin h)          = (h ! l, FHM fin (HM.insert l a h))
+  -- below assumes that both isos are the same.
+  -- furthermore, since the maps are total, intersection is total too
+  zipWith g (FHM f1 h1) (FHM _ h2) = FHM f1 (HM.intersectionWith g h1 h2)
+  smap g (FHM f h)                 = FHM f (HM.map g h)
+  append (FHM f1 h1) (FHM f2 h2)   = 
+      FHM (finite_Either f1 f2) (HM.union (rekey Left h1) (rekey Right h2))
+
+{-
+  initialize h          = h
+-}
+-}
