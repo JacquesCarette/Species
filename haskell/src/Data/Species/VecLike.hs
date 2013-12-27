0491f59acebed47d3cccc0d6ea57502712ebf75d
Jacques Carette <carette@mcmaster.ca>
2013-12-27 16:22:26 -0500

    add take' which is like take but works for arbitrary l, given Finite l.  Which basically shows just how powerful a Finite proof really is.  Way too powerful, really.  Also stub of an implementation of the classic filter function in terms of the Species one, needs to be completed.

--- haskell/src/Data/Species/VecLike.hs
+++ haskell/src/Data/Species/VecLike.hs
@@ -11,6 +11,7 @@
 
 module Data.Species.VecLike where
 
+import           Prelude hiding (filter)
 import           Control.Lens (iso)
 import           Data.Type.Equality
 
@@ -25,6 +26,7 @@ import           Data.Type.Nat.Inequality
 import           Data.Type.Nat.Minus
 import qualified Data.Storage             as S
 import qualified Data.Set.Abstract        as Set
+import qualified Data.Species.List        as L
 
 
 -- This requires quite a lot of inputs to 'work', but it does.
@@ -42,13 +44,42 @@ take (Struct f i) qq n pf =
         Refl -> Struct (cprod_ f (part_ sn sm isom)) i
           where isom :: Either (F.Fin n) (F.Fin m) <-> F.Fin q
                 isom = iso (finSum n m) (finSum' n m)
-                sn = N.natty n $ Set.enumerate $ finite_Fin
-                sm = N.natty m $ Set.enumerate $ finite_Fin
+                sn = N.natty n $ Set.enumerate finite_Fin
+                sm = N.natty m $ Set.enumerate finite_Fin
 
--- We need an Enumerable constraint
-filter :: (S.Storage s, Set.Enumerable l, Eq l) => Sp f s l a -> (a -> Bool) -> Sp (f # Part) s l a
+-- As alluded to above, given a Finite l, we can actually "take" from
+-- an arbitrary Species!  This shows the inherent danger of explicit
+-- finiteness proofs for label sets: they contain quite a bit more information
+-- then just 'finiteness'.
+take' :: forall a f l n s. 
+  Sp f s l a -> N.SNat (Size l) -> N.SNat n -> (n <= Size l) -> Finite l
+     -> Sp (f # Part) s l a
+take' (Struct f i) sizl n pf (F finl) =
+  case minus sizl n pf of
+    Minus (m :: N.SNat m) Refl ->
+      case N.plusComm m n of
+        Refl -> Struct (cprod_ f (part_ sn sm (isom . finl))) i
+          where isom :: Either (F.Fin n) (F.Fin m) <-> F.Fin (N.Plus n m)
+                isom = iso (finSum n m) (finSum' n m)
+                sn = N.natty n $ Set.enumerate finite_Fin
+                sm = N.natty m $ Set.enumerate finite_Fin
+
+-- We can do this with an Enumerable constraint, which is less `leaky'
+-- than a Finite proof.
+filter :: (S.Storage s, Set.Enumerable l, Eq l) => 
+          Sp f s l a -> (a -> Bool) -> Sp (f # Part) s l a
 filter (Struct f stor) p = Struct (cprod_ f k) stor
   where sel = S.smap p stor
         k = part_ Set.enumS Set.enumS
                   (iso (\l -> case l of {Left a -> a; Right a -> a}) 
                        (\l -> if (S.index sel l) then Left l else Right l) )
+
+-- One question still nags: is the above really filter?  In other words,
+-- could we implement Prelude.filter with the above?
+{-
+filter' :: forall a. (a -> Bool) -> [a] -> [a]
+filter' pr l = undefined
+  where sp :: Sp L.L (->) l a -- pick an explicit Storage
+        sp = case (L.fromList l) of SpEx s -> s
+        res = filter sp pr
+-}
