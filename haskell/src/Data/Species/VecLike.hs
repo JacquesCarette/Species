ab3018eb8ac516392299af91a3fc4bc778fdb06c
Jacques Carette <carette@mcmaster.ca>
2013-12-29 16:57:36 -0500

    and the long sought-after victory: we can fully implement filter (as in the prelude) using all this machinery. The point is that most of the work is at the level of the labels, not the data itself.  This is quite reminescent of some work presented at ICFP on making some Haskell code be parallelizable by splitting the work between what has to be done locally on data and what is on vectors-of-flags.

--- haskell/src/Data/Species/VecLike.hs
+++ haskell/src/Data/Species/VecLike.hs
@@ -12,7 +12,7 @@
 module Data.Species.VecLike where
 
 import           Prelude hiding (filter)
-import           Control.Lens (iso)
+import           Control.Lens (iso,view,from)
 import           Data.Type.Equality
 
 import qualified Data.Fin                 as F
@@ -27,7 +27,9 @@ import           Data.Type.Nat.Minus
 import qualified Data.Storage             as S
 import qualified Data.Set.Abstract        as Set
 import qualified Data.Species.List        as L
-
+import qualified Data.Vec                 as Vec
+import           Data.Species.Examples
+import           Data.Species.Elim
 
 -- This requires quite a lot of inputs to 'work', but it does.
 -- Given a species, its allocated size, the size of prefix you want to take,
@@ -76,10 +78,34 @@ filter (Struct f stor) p = Struct (cprod_ f k) stor
 
 -- One question still nags: is the above really filter?  In other words,
 -- could we implement Prelude.filter with the above?
-{-
+-- let's try to implement the 'back half' first: given a species of 
+-- shape L#Part, extract a filtered list
+extract :: (S.LabelledStorage s, Set.Enumerable l, Eq l) => 
+    Sp (L.L # Part) s l a -> [a]
+extract sp = 
+  -- because of existential, it is very hard to rewrite the code below
+  -- in a less `inline' style.
+  let (lsp, part) = decompL sp
+  in gelim (L.gelimList [] 
+      (\(l,a) r -> 
+        case part of 
+          Prod _ _ eiso ->
+            case view (from eiso) l of
+              Left _  -> a : r
+              Right _ ->     r  )) lsp
+
+-- Victory!
+-- The *code* below is quite ugly, but that is because we have to have a way
+-- to be explicit about all our choices.  Our setup is very polymorphic,
+-- and we need to pin all these things down to get a runnable routine.
 filter' :: forall a. (a -> Bool) -> [a] -> [a]
-filter' pr l = undefined
-  where sp :: Sp L.L (->) l a -- pick an explicit Storage
-        sp = case (L.fromList l) of SpEx s -> s
-        res = filter sp pr
--}
+filter' p lst = 
+  case Vec.fromList lst of
+    Vec.SomeVec v -> 
+      let n = Vec.size v
+          sp = fromVec v
+          e :: forall n. N.Natural n => Sp (L.L # Part) (->) (F.Fin n) a -> [a]
+          e = extract
+          f :: forall n. N.Natural n => Sp L.L (->) (F.Fin n) a -> (a -> Bool) -> Sp (L.L # Part) (->) (F.Fin n) a
+          f = filter
+      in N.natty n $ e $ f sp p
